c34fcd72c0cb6929141c0f3686b20ed67860f789
sumeriocode <sumeriocode@gmail.com>
2023-06-09 17:02:10 -0400

    modify .env

--- src/api/health/health.module.ts
+++ src/api/health/health.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Todo } from '../todo/todo.scheme';
 import { HealthController } from './health.controller';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
